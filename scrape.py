a0a970a3ba6ba8dcb9b757d896070e5606079ac0
Samuel Odongo <samieodd@gmail.com>
2020-08-30 01:16:00 +0300

    Select specific attributs and assign to variables

--- scrape.py
+++ scrape.py
@@ -2,6 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 
 response = requests.get('https://news.ycombinator.com/news')
-
 soup = BeautifulSoup(response.text, 'html.parser')
-print(soup)
+links = soup.select('.storylink')
+votes = soup.select('.score')
