29f79535c7abd5fcc149c922322220c65f166800
Samuel Odongo <samieodd@gmail.com>
2020-08-30 01:40:05 +0300

    Expanded function to avoid articles with 0 votes

--- scrape.py
+++ scrape.py
@@ -4,16 +4,19 @@ from bs4 import BeautifulSoup
 response = requests.get('https://news.ycombinator.com/news')
 soup = BeautifulSoup(response.text, 'html.parser')
 links = soup.select('.storylink')
-votes = soup.select('.score')
+subtext = soup.select('.subtext')
 
-def create_custom_hn(links, votes):
+def create_custom_hn(links, subtext):
     hn = []
     for index, item in enumerate(links):
         title = links[index].getText()
         href = links[index].get('href', None)
-        points = int(votes[index].getText().replace(' points', ''))
-        print(points)
-        hn.append({'title': title, 'link': href})
+        vote = subtext[index].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', ''))
+            if points > 99:
+                hn.append({'title': title, 'link': href, 'votes': points,})
     return hn
 
-create_custom_hn(links, votes)
+import pprint
+pprint.pprint(create_custom_hn(links, subtext))
