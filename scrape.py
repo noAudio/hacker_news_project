236d8875a6649ba5394ac0b5d088e5d6a4d3b77a
Samuel Odongo <samieodd@gmail.com>
2020-08-30 01:09:09 +0300

    Create soup object with BeautifulSoup

--- scrape.py
+++ scrape.py
@@ -2,4 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 
 response = requests.get('https://news.ycombinator.com/news')
-print(response.text)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+print(soup)
