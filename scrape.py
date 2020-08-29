adc1a143bb8268ba8492100aed5a0cd7d25b5897
Samuel Odongo <samieodd@gmail.com>
2020-08-30 01:27:11 +0300

    Created function to get specific info from soup

--- scrape.py
+++ scrape.py
@@ -5,3 +5,14 @@ response = requests.get('https://news.ycombinator.com/news')
 soup = BeautifulSoup(response.text, 'html.parser')
 links = soup.select('.storylink')
 votes = soup.select('.score')
+
+def create_custom_hn(links, votes):
+    hn = []
+    for index, item in enumerate(links):
+        title = links[index].getText()
+        href = links[index].get('href', None)
+        points = votes[index].getText()
+        hn.append({'title': title, 'link': href})
+    return hn
+
+create_custom_hn(links, votes)
